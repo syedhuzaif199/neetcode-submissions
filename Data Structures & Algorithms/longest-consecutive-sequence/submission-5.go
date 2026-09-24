6918046300f4c6e22aebe6948d8f9c8185a7ff7c
Syed Huzaif <72594863+syedhuzaif199@users.noreply.github.com>
2026-09-24 09:43:02 +0530

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.go
@@ -0,0 +1,25 @@
+func longestConsecutive(nums []int) int {
+	longest := 0
+	m := map[int]struct{}{}
+
+	for _, num := range nums {
+		m[num] = struct{}{}
+	}
+
+	for _, num := range nums {
+		if _, exists := m[num-1]; !exists {
+			count := 1
+			for {
+				_, ok := m[num+1]
+				if !ok {
+					break
+				}
+				count += 1
+				num += 1
+			}
+			longest = max(longest, count)
+		}
+	}
+
+	return longest
+}
